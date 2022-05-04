2c274414f12d2f673647fb658e9ec17495c89b41
stacy <ada.davis27@gmail.com>
2022-05-05 01:07:13 +0300

    changes

--- src/features/TodolistsList/TodolistsList.tsx
+++ src/features/TodolistsList/TodolistsList.tsx
@@ -28,7 +28,7 @@ export const TodolistsList = () => {
             {todolists.map(({id}) => <Grid item key={id} style={{paddingRight: '40px'}}>
                     <Paper elevation={3} style={{
                         padding: '10px',
-                        minWidth: '370px', maxWidth: '370px',
+                        minWidth: '340px', maxWidth: '340px',
                     }}>
                         <Todolist Tid={id}/>
                     </Paper>
