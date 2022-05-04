7b37a6ec0183b474735fec7bfa573df849b26dec
stacy <ada.davis27@gmail.com>
2022-05-04 12:49:03 +0300

    changes

--- src/api/todolists-api.ts
+++ src/api/todolists-api.ts
@@ -78,12 +78,7 @@ export type TaskType = {
     deadline: string
     addedDate: string
 }
-export enum TaskStatuses {
-    New = 0,
-    InProgress = 1,
-    Completed = 2,
-    Draft = 3
-}
+
 export enum TaskPriorities {
     Low = 0,
     Middle = 1,
